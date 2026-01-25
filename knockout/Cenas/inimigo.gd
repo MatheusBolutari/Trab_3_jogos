a52d4b704f8902fdbdbf7493cf2e83c82413450d
Arthur <11691831662@estudante.ufjf.br>
2026-01-25 11:49:00 -0300

    Adicionado 'pathfindind' basico, e inimigo sempre olha para o player

--- knockout/Cenas/inimigo.gd
+++ knockout/Cenas/inimigo.gd
@@ -2,13 +2,20 @@ extends CharacterBody3D
 
 @export var velocidade: float
 @onready var animation_player: AnimationPlayer = $"3DGodotRobot/AnimationPlayer"
+@onready var player: CharacterBody3D = $"../Player"
 
 
 func _physics_process(delta: float) -> void:
-	if randf() > 0.5:
-		velocity += velocidade * Vector3.FORWARD
-		animation_player.play("Run")
-	else:
-		velocity += velocidade * Vector3.BACK
-		animation_player.play("Run")
+	if player:
+		$NavigationAgent3D.target_position = player.position
+		if !$NavigationAgent3D.is_navigation_finished():
+			velocity += velocidade * position.direction_to($NavigationAgent3D.target_position).normalized() * Vector3(1,0,1)
+			animation_player.play("Run")
+		look_at(player.position)
+	#if randf() > 0.5:
+		#velocity += velocidade * Vector3.FORWARD
+		#animation_player.play("Run")
+	#else:
+		#velocity += velocidade * Vector3.BACK
+		#animation_player.play("Run")
 	move_and_slide()
