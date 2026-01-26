ec964d12c6a2e99fd6ebe95346c80ba8a1985f3a
Arthur <11691831662@estudante.ufjf.br>
2026-01-26 13:28:14 -0300

    Minor fixes

--- knockout/Scripts/inimigo.gd
+++ knockout/Scripts/inimigo.gd
@@ -9,23 +9,25 @@ var target_pos : Vector3
 signal hit
 
 func _physics_process(delta: float) -> void:
+	#var v = Vector3.ZERO
 	if player:
 		target_pos = player.position
 		$NavigationAgent3D.target_position = target_pos
-		if !$NavigationAgent3D.is_navigation_finished() and position.distance_to(target_pos) >= 2:
-			velocity += velocidade * position.direction_to($NavigationAgent3D.target_position).normalized() * Vector3(1,0,1)
+		if !$NavigationAgent3D.is_navigation_finished():
+			velocity = velocidade * (position.direction_to($NavigationAgent3D.get_next_path_position()).normalized()) * Vector3(1,0,1)
 			animation_player.play("Run")
-		look_at(player.position)
+			look_at(target_pos)
 		if position.distance_to(target_pos) < 2:
 			animation_player.play("Attack1")
 	elif !player:
 		if randf() > 0.5:
-			velocity += velocidade * Vector3.FORWARD
+			velocity = velocidade * Vector3.FORWARD
 			animation_player.play("Run")
 		else:
-			velocity += velocidade * Vector3.BACK
+			velocity = velocidade * Vector3.BACK
 			animation_player.play("Run")
 	move_and_slide()
+	#translate(v)
 
 func _on_colision_controller_attack(arg : String) -> void:
 	if arg == "dano":
