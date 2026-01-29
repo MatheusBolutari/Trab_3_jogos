55ed03a4ad2c372abfb278804bd70e5d46b9a5c0
Arthur <11691831662@estudante.ufjf.br>
2026-01-29 18:40:44 -0300

    Finalização do projeto

--- knockout/Scripts/gerencia_som.gd
+++ knockout/Scripts/gerencia_som.gd
@@ -1,11 +1,13 @@
 extends Node
 
-func _on_gerencia_bracos_colisao(arg: String, _dano: int) -> void:
-	if arg == 'soco':
+func _on_gerencia_bracos_colisao(arg: String, dano: int) -> void:
+	if arg == 'soco' and dano < 100:
 		if randf() < 0.5:
 			$Som_Soco1.play()
 		else :
 			$Som_Soco2.play()
+	elif arg == 'soco' and dano == 100:
+		$Som_Soco9.play()
 
 
 func _on_inimigo_attack_sound() -> void:
